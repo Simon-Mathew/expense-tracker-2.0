0fbc59b77a5f22c36332d2e7ed79b8e719acd8db
Simon Mathew Cyriac <simonmathewcyriac2@gmail.com>
2026-05-03 12:12:28 +1000

    create transaction page and add the routes to display the page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Dashboard } from '../app/pages/dashboard/Dashboard';
 import { NewTransactionForm } from './pages/new-transaction-form/new-transaction-form';
 import { Budgets } from '../app/pages/budgets/budgets';
+import { TransactionPage } from './pages/transaction-page/transaction-page';
 
 export const routes: Routes = [
   { path: '', component: Dashboard },
   { path: 'dashboard', component: Dashboard },
   { path: 'new-transaction-form', component: NewTransactionForm },
   { path: 'budgets', component: Budgets },
+  { path: 'transaction-page', component: TransactionPage },
 ];
